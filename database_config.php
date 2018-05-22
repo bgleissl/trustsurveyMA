c5996ed9abaff5cc1851eaec1addb04da64b1e6c
bgleissl <ga45zom@mytum.de>
2018-05-22 19:58:09 +0200

    Delete database_config.php

--- database_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $servername = "mysql-web2.sql.lrz.de"; // database is on this server, maybe this needs: mysql-web2.sql.lrz.de
-  $port = 3306; // default
-  $username = "di52nuq"; // not sure about this one yet but it worked in phpMyAdmin
-  $password = "6KRtZMloJIt5"; // so far: standard password from pw file
-  $dbname = "di52nuqdb01"; // database for trustservey server
-  $table = "MA_Pilot"; // to do 
-?>
